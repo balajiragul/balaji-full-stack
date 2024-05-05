8572d7a6fa73e28ebc3607bdae17d3bfc372f086
balajiragul <120189202+balajiragul@users.noreply.github.com>
2024-05-05 21:25:48 +0530

    Add files via upload

---
+++ 01.Registration Form/app.js
@@ -0,0 +1,73 @@
+// //let name = "balaji";
+// //var whatDoYouWannaBecomeInYourLife = "programmer";
+// //var gender = "male";
+// //var twitterHandle = "no";
+// //console.log(name);
+// //console.log(whatDoYouWannaBecomeInYourLife);
+// //console.log(gender);
+// //console.log(twitterHandle);
+
+// //let firstFavNum = 10;
+// //let secondFavNum = 20;
+// //console.log(firstFavNum + secondFavNum);
+// //console.log(firstFavNum - secondFavNum);
+// //console.log(firstFavNum * secondFavNum);
+// //console.log(firstFavNum % secondFavNum);
+// //console.log(firstFavNum **secondFavNum);
+
+// //let isJsProgrammingLanguage = true;
+// //let isJsHard = false;
+// //let favNumb = 5;
+
+// //console.log(isJsProgrammingLanguage);
+// //console.log(isJsHard);
+// //console.log(favNumb + undefined);
+
+// //console.log(10 === 10);
+// //console.log(10 !== 10);
+
+// // let firstFavNum = 10;
+// // let secondFavNum = 20;
+// // console.log(firstFavNum < secondFavNum);
+// // console.log(firstFavNum > secondFavNum);
+// // console.log(firstFavNum >= secondFavNum);
+// // console.log(firstFavNum <= secondFavNum);
+// // console.log(firstFavNum === secondFavNum);
+// // console.log(firstFavNum == secondFavNum);
+// // console.log(firstFavNum !== secondFavNum);
+// // console.log(firstFavNum != secondFavNum);
+
+
+// //Type conversion 
+// // let money = 50;
+// let money = "50.245";
+// //-convert string to number
+// // money = parseInt(money); --> method 1
+// // money = +money; --> method 2
+// // money = Number(money); --> method 3
+
+// //-convert number to string 
+// // money = money.toString(); --> method 1
+// // money = String(money); --> method 2
+
+// //-convert string to decimal
+// // money = parseFloat(money);
+
+// // console.log(money);
+// // console.log(typeof money);
+
+// //if else condition
+
+// let a = 10;
+// let b = 10;
+
+// if(a > b){
+//        console.log("a is greater than b");
+// }
+// else if(a < b){
+//     console.log("a is less than b");
+// }
+// else{
+//     console.log("a is equal to b");
+// }
+
